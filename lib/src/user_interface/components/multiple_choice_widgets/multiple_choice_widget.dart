bd5c047b90cbffe0ae15ec64a66f55513b475f12
Shourya Bansal <shouryabansal2012@gmail.com>
2021-01-16 11:23:04 -0500

    Start Creating Parent Widget

---
+++ lib/src/user_interface/components/multiple_choice_widgets/multiple_choice_widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+import '../../../business_logic/models/question.dart';
+import '../../../user_interface/theme/style.dart';
+
+class MultipleChoiceWidget extends StatefulWidget {
+  @override
+  _MultipleChoiceWidgetState createState() => _MultipleChoiceWidgetState();
+}
+
+class _MultipleChoiceWidgetState extends State<MultipleChoiceWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
