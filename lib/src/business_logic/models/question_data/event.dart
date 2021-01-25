e5d081adb8dae6ebfe35a7728dba1ce2d354f625
Shourya Bansal <shouryabansal2012@gmail.com>
2021-01-25 14:58:27 -0500

    Move Event to its own Class

--- lib/src/business_logic/models/question_data/event.dart
+++ lib/src/business_logic/models/question_data/event.dart
@@ -1,44 +1,137 @@
-enum Event {
-  accountingI,
-  accountingII,
-  advertising,
-  agribusiness,
-  bankingAndFinancialSystems,
-  businessCalculations,
-  businessCommunications,
-  businessLaw,
-  computerApplications,
-  computerProblemSolving,
-  cyberSecurity,
-  databaseDesignAndApplications,
-  economics,
-  entrepreneurship,
-  futureBusinessLeader,
-  healthCareAdministration,
-  helpDesk,
-  hospitalityAndEventManagement,
-  insuranceAndRiskManagement,
-  internationalBusiness,
-  introductionToBusinessCommunication,
-  introductionToBusiness,
-  introductionToBusinessProcedures,
-  introductionToEventPlanning,
-  introductionToFbla,
-  introductionToFinancialMath,
-  introductionToInformationTechnology,
-  introductionToParliamentaryProcedure,
-  journalism,
-  managementDecisionMaking,
-  managementInformationSystems,
-  marketing,
-  networkDesign,
-  networkingInfrastructures,
-  organizationalLeadership,
-  parliamentaryProcedure,
-  personalFinance,
-  politicalScience,
-  securitiesAndInvestments,
-  sportsAndEntertainmentManagement,
-  spreadsheetApplications,
-  wordProcessing,
+class Event {
+  final String name;
+
+  const Event._(this.name);
+
+  @override
+  String toString() {
+    return "None";
+  }
+
+  static const Event accountingI = Event._(
+    "Accounting 1",
+  );
+  static const Event accountingII = Event._(
+    "Accounting 2",
+  );
+  static const Event advertising = Event._(
+    "Advertising",
+  );
+  static const Event agribusiness = Event._(
+    "Agribusiness",
+  );
+  static const Event bankingAndFinancialSystems = Event._(
+    "Banking & Financial Systems",
+  );
+  static const Event businessCalculations = Event._(
+    "Business Calculations",
+  );
+  static const Event businessCommunications = Event._(
+    "Business Communication",
+  );
+  static const Event businessLaw = Event._(
+    "Business Law",
+  );
+  static const Event computerApplications = Event._(
+    "Computer Applications",
+  );
+  static const Event computerProblemSolving = Event._(
+    "Computer Problem Solving",
+  );
+  static const Event cyberSecurity = Event._(
+    "Cyber Security",
+  );
+  static const Event databaseDesignAndApplications = Event._(
+    "Database Design and Applications",
+  );
+  static const Event economics = Event._(
+    "Economics",
+  );
+  static const Event entrepreneurship = Event._(
+    "Entrepreneurship",
+  );
+  static const Event futureBusinessLeader = Event._(
+    "Future Business Leader",
+  );
+  static const Event healthCareAdministration = Event._(
+    "Health Care Administartion",
+  );
+  static const Event helpDesk = Event._(
+    "Help Desk",
+  );
+  static const Event hospitalityAndEventManagement = Event._(
+    "Hospitality and Event Management",
+  );
+  static const Event insuranceAndRiskManagement = Event._(
+    "Insurance and Risk Management",
+  );
+  static const Event internationalBusiness = Event._(
+    "International Business",
+  );
+  static const Event introductionToBusinessCommunication = Event._(
+    "Introduction to Business Communication",
+  );
+  static const Event introductionToBusiness = Event._(
+    "Introduction to Business",
+  );
+  static const Event introductionToBusinessProcedures = Event._(
+    "Introduction to Business Procedures",
+  );
+  static const Event introductionToEventPlanning = Event._(
+    "Introduction to Event Planning",
+  );
+  static const Event introductionToFbla = Event._(
+    "Introduction to FBLA",
+  );
+  static const Event introductionToFinancialMath = Event._(
+    "Introduction to Financial Math",
+  );
+  static const Event introductionToInformationTechnology = Event._(
+    "Introduction to Information Technology",
+  );
+  static const Event introductionToParliamentaryProcedure = Event._(
+    "Introduction to Parliamentary Procedure",
+  );
+  static const Event journalism = Event._(
+    "Journalism",
+  );
+  static const Event managementDecisionMaking = Event._(
+    "Management Decision Making",
+  );
+  static const Event managementInformationSystems = Event._(
+    "Management Information Systems",
+  );
+  static const Event marketing = Event._(
+    "Marketing",
+  );
+  static const Event networkDesign = Event._(
+    "Network Design",
+  );
+  static const Event networkingInfrastructures = Event._(
+    "Networking Infrastructures",
+  );
+  static const Event organizationalLeadership = Event._(
+    "organizational Leadership",
+  );
+  static const Event parliamentaryProcedure = Event._(
+    "Parliamentary Procedure",
+  );
+  static const Event personalFinance = Event._(
+    "Personal Finance",
+  );
+  static const Event politicalScience = Event._(
+    "Political Science",
+  );
+  static const Event securitiesAndInvestments = Event._(
+    "Securities and Investments",
+  );
+  static const Event sportsAndEntertainmentManagement = Event._(
+    "Sports and Entertainment Management",
+  );
+  static const Event spreadsheetApplications = Event._(
+    "Spreadsheet Applications",
+  );
+  static const Event wordProcessing = Event._(
+    "Word Processing",
+  );
 }
--- lib/src/user_interface/screens/question_screen.dart
+++ lib/src/user_interface/screens/question_screen.dart
@@ -1,13 +1,61 @@
 import 'package:flutter/material.dart';
 
+import '../../business_logic/models/question.dart';
+import '../components/question_widgets.dart';
+
 class QuestionScreen extends StatefulWidget {
   @override
   _QuestionScreenState createState() => _QuestionScreenState();
 }
 
 class _QuestionScreenState extends State<QuestionScreen> {
+  final List<QuestionWidget> questionWidgetList = [];
+
+  void nextQuestion() {}
+
+  void previousQuestion() {}
+
+  _QuestionScreenState() {
+    questionWidgetList
+        .addAll(_Questions(nextQuestion, previousQuestion).getQuestions());
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
   }
+}
+
+// https://www.fbla-pbl.org/media/accounting_i_online_ref_guide.pdf
+class _Questions {
+  final void Function() next, previous;
+
+  _Questions(this.next, this.previous);
+
+  List<QuestionWidget> getQuestions() {
+    return <QuestionWidget>[
+      MultipleChoiceWidget(
+        MultipleChoiceQuestionData(
+          question:
+              'A special journal used to record only cash recept transactions is: ',
+          choices: AnswerChoice.of([
+            'a purchases journal',
+            'a cash receipts journal',
+            'a cash journal',
+            'a sales journal',
+          ], 'a cash receipts journal'),
+          id: Event.accountingI.withIdString('MC0001'),
+        ),
+        1,
+        nextQuestion: next,
+        previousQuestion: previous,
+      ),
+    ];
+  }
+}
+
+extension EventNumber on Event {
+  String get id => this.index as String;
+
+  String withIdString(String id) => '${this.id}-$id';
 }
