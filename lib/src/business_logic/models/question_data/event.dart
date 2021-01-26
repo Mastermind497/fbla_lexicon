687c73ccef13af7a2f929021c1a65d26e0ff4da0
Shourya Bansal <shouryabansal2012@gmail.com>
2021-01-26 18:26:57 -0500

    Add nameId field to Event

--- lib/src/business_logic/models/question_data/event.dart
+++ lib/src/business_logic/models/question_data/event.dart
@@ -1,7 +1,8 @@
 class Event {
   final String name;
+  final String nameId;
 
-  const Event._(this.name);
+  const Event._(this.name, this.nameId);
 
   @override
   String toString() {
@@ -10,131 +11,174 @@ class Event {
 
   static const Event accountingI = Event._(
     "Accounting 1",
+    "acc1",
   );
   static const Event accountingII = Event._(
     "Accounting 2",
+    "acc2",
   );
   static const Event advertising = Event._(
     "Advertising",
+    "advt",
   );
   static const Event agribusiness = Event._(
     "Agribusiness",
+    "agri",
   );
   static const Event bankingAndFinancialSystems = Event._(
     "Banking & Financial Systems",
+    "bafs",
   );
   static const Event businessCalculations = Event._(
     "Business Calculations",
+    "bcal",
   );
   static const Event businessCommunications = Event._(
     "Business Communication",
+    "bcom",
   );
   static const Event businessLaw = Event._(
     "Business Law",
+    "blaw",
   );
   static const Event computerApplications = Event._(
     "Computer Applications",
+    "coap",
   );
   static const Event computerProblemSolving = Event._(
     "Computer Problem Solving",
+    "cops",
   );
   static const Event cyberSecurity = Event._(
     "Cyber Security",
+    "cyse",
   );
   static const Event databaseDesignAndApplications = Event._(
     "Database Design and Applications",
+    "ddaa",
   );
   static const Event economics = Event._(
     "Economics",
+    "econ",
   );
   static const Event entrepreneurship = Event._(
     "Entrepreneurship",
+    "entr",
   );
   static const Event futureBusinessLeader = Event._(
     "Future Business Leader",
+    "fbla",
   );
   static const Event healthCareAdministration = Event._(
     "Health Care Administartion",
+    "hcad",
   );
   static const Event helpDesk = Event._(
     "Help Desk",
+    "hede",
   );
   static const Event hospitalityAndEventManagement = Event._(
     "Hospitality and Event Management",
+    "haem",
   );
   static const Event insuranceAndRiskManagement = Event._(
     "Insurance and Risk Management",
+    "iarm",
   );
   static const Event internationalBusiness = Event._(
     "International Business",
+    "ibus",
   );
   static const Event introductionToBusinessCommunication = Event._(
     "Introduction to Business Communication",
+    "itbc",
   );
   static const Event introductionToBusiness = Event._(
     "Introduction to Business",
+    "itbu",
   );
   static const Event introductionToBusinessProcedures = Event._(
     "Introduction to Business Procedures",
+    "itbp",
   );
   static const Event introductionToEventPlanning = Event._(
     "Introduction to Event Planning",
+    "itep",
   );
   static const Event introductionToFbla = Event._(
     "Introduction to FBLA",
+    "ifbl",
   );
   static const Event introductionToFinancialMath = Event._(
     "Introduction to Financial Math",
+    "itfm",
   );
   static const Event introductionToInformationTechnology = Event._(
     "Introduction to Information Technology",
+    "itit",
   );
   static const Event introductionToParliamentaryProcedure = Event._(
     "Introduction to Parliamentary Procedure",
+    "itpp",
   );
   static const Event journalism = Event._(
     "Journalism",
+    "jour",
   );
   static const Event managementDecisionMaking = Event._(
     "Management Decision Making",
+    "made",
   );
   static const Event managementInformationSystems = Event._(
     "Management Information Systems",
+    "mais",
   );
   static const Event marketing = Event._(
     "Marketing",
+    "mark",
   );
   static const Event networkDesign = Event._(
     "Network Design",
+    "netd",
   );
   static const Event networkingInfrastructures = Event._(
     "Networking Infrastructures",
+    "ninf",
   );
   static const Event organizationalLeadership = Event._(
-    "organizational Leadership",
+    "Organizational Leadership",
+    "orgl",
   );
   static const Event parliamentaryProcedure = Event._(
     "Parliamentary Procedure",
+    "papr",
   );
   static const Event personalFinance = Event._(
     "Personal Finance",
+    "pefi",
   );
   static const Event politicalScience = Event._(
     "Political Science",
+    "posi",
   );
   static const Event securitiesAndInvestments = Event._(
     "Securities and Investments",
+    "seai",
   );
   static const Event sportsAndEntertainmentManagement = Event._(
     "Sports and Entertainment Management",
+    "saem",
   );
   static const Event spreadsheetApplications = Event._(
     "Spreadsheet Applications",
+    "spap",
   );
   static const Event wordProcessing = Event._(
     "Word Processing",
+    "word",
   );
   static const Event none = Event._(
     "None",
+    "none",
   );
 }
