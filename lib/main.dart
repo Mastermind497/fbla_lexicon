6ef396dd1429610b61cf00d6219f2b3195acb9dd
Shourya Bansal <shouryabansal2012@gmail.com>
2021-01-27 15:48:16 -0500

    Create an App Functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,17 @@
 import 'package:flutter/material.dart';
 
-import 'src/business_logic/models/question.dart';
-import 'src/user_interface/components/question_widgets.dart';
+import 'src/app.dart';
+import 'src/user_interface/theme/style.dart';
 
-void main() {
-  runApp(MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      theme: appData(),
+      home: FblaLexicon(),
     );
   }
 }
@@ -30,17 +26,9 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  var question = FreeResponseQuestionData(
-    id: 'id',
-    question: 'What is the best fruit',
-    answer: 'chocolate',
-  );
-
   var questionWidget;
 
-  _MyHomePageState() {
-    questionWidget = FreeResponseWidget(question, 1);
-  }
+  _MyHomePageState();
 
   @override
   Widget build(BuildContext context) {
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,1 +1,15 @@
+import 'package:flutter/material.dart';
 
+import 'user_interface/screens/question_screen.dart';
+
+class FblaLexicon extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('FBLA Lexicon'),
+      ),
+      body: QuestionScreen(),
+    );
+  }
+}
