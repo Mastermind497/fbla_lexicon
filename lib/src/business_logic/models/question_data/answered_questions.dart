9040fc5706c0cf3e18fc6b4e393bfb6939982e63
Shourya Bansal <shouryabansal2012@gmail.com>
2021-02-01 17:10:09 -0500

    Fix AnsweredQuestion Implementations

--- lib/src/business_logic/models/question_data/answered_questions.dart
+++ lib/src/business_logic/models/question_data/answered_questions.dart
@@ -5,100 +5,108 @@ import 'question_foundation.dart';
 
 class AnsweredMultipleChoiceQuestion implements AnsweredQuestion {
   final String _id;
-  final AnswerChoice _picked;
+  final AnswerChoice _chosen;
   final AnswerChoice _correctAnswer;
   final Event _event;
 
   const AnsweredMultipleChoiceQuestion({
     @required String id,
-    @required AnswerChoice picked,
+    @required AnswerChoice chosen,
     @required AnswerChoice correctAnswer,
     @required Event event,
   })  : _id = id,
-        _picked = picked,
+        _chosen = chosen,
         _correctAnswer = correctAnswer,
         _event = event;
 
   String get id => _id;
 
-  AnswerChoice get picked => _picked;
+  AnswerChoice get chosen => _chosen;
 
-  bool get isCorrect => _picked == _correctAnswer;
+  bool get isCorrect => _chosen == _correctAnswer;
 
   Event get event => _event;
 }
 
-class AnsweredDropdownQuestion implements AnsweredQuestion {
+class AnsweredMultipleResponseQuestion implements AnsweredQuestion {
   final String _id;
-  final AnswerChoice _picked;
-  final AnswerChoice _correctAnswer;
+  final List<AnswerChoice> _chosen;
+  final List<AnswerChoice> _correctAnswer;
   final Event _event;
 
-  const AnsweredDropdownQuestion({
+  const AnsweredMultipleResponseQuestion({
     @required String id,
-    @required AnswerChoice picked,
-    @required AnswerChoice correctAnswer,
+    @required List<AnswerChoice> chosen,
+    @required List<AnswerChoice> correctAnswer,
     @required Event event,
   })  : _id = id,
-        _picked = picked,
+        _chosen = chosen,
         _correctAnswer = correctAnswer,
         _event = event;
 
   String get id => _id;
 
-  AnswerChoice get picked => _picked;
+  List<AnswerChoice> get chosen => _chosen;
 
-  bool get isCorrect => _picked == _correctAnswer;
+  bool get isCorrect {
+    if (_chosen != null && _chosen.length == _correctAnswer.length) {
+      _chosen.forEach((element) {
+        if (!element.isCorrect) return false;
+      });
+      return true;
+    } else
+      return false;
+  }
 
   Event get event => _event;
 }
 
 class AnsweredTrueFalseQuestion implements AnsweredQuestion {
   final String _id;
-  final bool _picked;
+  final bool _chosen;
   final bool _correctAnswer;
   final Event _event;
 
   const AnsweredTrueFalseQuestion({
     @required String id,
-    @required bool picked,
+    @required bool chosen,
     @required bool correctAnswer,
     @required Event event,
   })  : _id = id,
-        _picked = picked,
+        _chosen = chosen,
         _correctAnswer = correctAnswer,
         _event = event;
 
   String get id => _id;
 
-  bool get picked => _picked;
+  bool get chosen => _chosen;
 
-  bool get isCorrect => _picked == _correctAnswer;
+  bool get isCorrect => _chosen == _correctAnswer;
 
   Event get event => _event;
 }
 
 class AnsweredFreeResponseQuestion implements AnsweredQuestion {
   final String _id;
-  final String _picked;
+  final String _chosen;
   final String _correctAnswer;
   final Event _event;
 
   const AnsweredFreeResponseQuestion({
     @required String id,
-    @required String picked,
+    @required String chosen,
     @required String correctAnswer,
     @required Event event,
   })  : _id = id,
-        _picked = picked,
+        _chosen = chosen,
         _correctAnswer = correctAnswer,
         _event = event;
 
   String get id => _id;
 
-  String get picked => _picked;
+  String get chosen => _chosen;
 
-  bool get isCorrect => _picked == _correctAnswer;
+  bool get isCorrect => _chosen == _correctAnswer;
 
   Event get event => _event;
 }
